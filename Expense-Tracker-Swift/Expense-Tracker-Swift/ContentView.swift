22393a0964d240c58a6f2a964123795791d9f97f
Sreekutty Maya <sreekuttystar@gmail.com>
2025-01-06 17:48:56 +0400

    expense tracker project setup

---
+++ Expense-Tracker-Swift/Expense-Tracker-Swift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Expense-Tracker-Swift
+//
+//  Created by Sreekutty Maya on 01/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Expense-Tracker-Swift/Expense-Tracker-Swift/Expense_Tracker_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_Tracker_SwiftApp.swift
+//  Expense-Tracker-Swift
+//
+//  Created by Sreekutty Maya on 01/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct Expense_Tracker_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
